42426ceb841de7e7cbaac2857842bfec6b3da4ed
OCANDABEAST <81494306+OCANDABEAST@users.noreply.github.com>
2022-01-16 16:52:24 -0500

    Add files via upload

---
+++ askey.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+root =Tk()
+root.title("Ascii")
+
+root.geometry("400x400")
+root.configure(background = 'sky blue')
+
+enter_word = Entry(root)
+enter_word.place(relx=0.5,rely=0.4,anchor=CENTER)
+
+label = Label(root, text = "Name In Ascill :",bg="light yellow", fg="black")
+
+def asciiConverter():
+ input_word=enter_word.get()
+ 
+ for letter in input_word :
+     label["text"]+=str(ord(letter))+ " "
+      
+btn=Button(root,text="Show name in Ascill",command=asciiConverter, bg='gold',fg ='black')
+btn.place(relx=0.5,rely=0.5,anchor=CENTER)
+      
+label.place(relx=0.5,rely=0.6,anchor=CENTER)
+
+
+
+root.mainloop()
